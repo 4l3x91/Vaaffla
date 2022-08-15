e94051096ead132fc2e298554441ec75595e1e78
4l3x <alexander.auermaa@gmail.com>
2022-08-15 02:11:47 +0200

    green waffle, gray waffle

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let bottomWord = "";
 let leftWord = "";
 let rightWord = "";
 let verticleMidWord = "";
-let wordArray = [];
+let correctWordArray = [];
 
 
 createWaffle();
@@ -26,16 +26,21 @@ function drop(e) {
   clone = e.target.cloneNode(true);
   console.log(e.target.id + ' replaced')
   let data = e.dataTransfer.getData("text");
+  let tileTarget;
 
   if (clone.id !== data) {
     let nodelist = document.getElementById("grid").childNodes;
     for (let i = 0; i < nodelist.length; i++) {
-      if (nodelist[i].id == data) dragindex = i;
+      if (nodelist[i].id == data) {
+        dragindex = i;
+        tileTarget = e.target;
+      }
     }
+    currentTile = Array.from(tileTarget.parentNode.children).indexOf(tileTarget);
     document.getElementById("grid").replaceChild(document.getElementById(data), e.target);
     document.getElementById("grid").insertBefore(clone, document.getElementById("grid").childNodes[dragindex]);
 
-    checkLetter(pickedUpLetter, e.target.id);
+    checkLetter(pickedUpLetter, currentTile);
   }
 }
 
@@ -43,8 +48,21 @@ function allowDrop(e) {
   e.preventDefault();
 }
 
-function checkLetter(letter, targetId) {
-    console.log(letter + " " + targetId)
+function checkLetter(letter, index) {
+  const grid = document.getElementById("grid").children;
+    let currentTile = grid[index];
+    console.log(currentTile)
+    if(letter == correctWordArray[index])
+    {
+      currentTile.classList.remove("wrong");
+      currentTile.classList.add("right");
+      console.log(letter + " is in the correct place!")
+    }
+    else {
+      currentTile.classList.remove("right");
+      currentTile.classList.add("wrong");
+      console.log(letter + " is not in the correct place!")
+    }
   }
 
 function createWaffle() {
@@ -96,11 +114,54 @@ function mapWaffle() {
       currentTile.innerHTML = mapContent(index);
     }
   }
-  console.log(wordArray)
+  setCorrectWordArray();
+  console.log(correctWordArray)
+  console.log(topWord)
+  console.log(rightWord)
+  console.log(leftWord)
+  console.log(middleWord)
+  console.log(verticleMidWord)
+  console.log(bottomWord)
   }
 
 function mapContent(index) {
-  wordArray = [
+  scrambledWordArray = [
+    topWord[1],
+    topWord[0],
+    topWord[3],
+    topWord[2],
+    leftWord[1],
+    middleWord[4],
+    "",
+    topWord[4],
+    "",
+    middleWord[0],
+    rightWord[1],
+    middleWord[2],
+    bottomWord[2],
+    bottomWord[3],
+    verticleMidWord[1],
+    leftWord[3],
+    "",
+    rightWord[3],
+    "",
+    verticleMidWord[3],
+    bottomWord[0],
+    middleWord[3],
+    bottomWord[1],
+    middleWord[1],
+  bottomWord[4]
+];
+  return scrambledWordArray[index];
+}
+
+function getRandomWord() {
+  const randomNumber = Math.floor(Math.random() * words.length);
+  return words[randomNumber];
+}
+
+function setCorrectWordArray() {
+  correctWordArray = [
     topWord[0],
     topWord[1],
     topWord[2],
@@ -119,18 +180,12 @@ function mapContent(index) {
     leftWord[3],
     "",
     verticleMidWord[3],
-    "",
-    rightWord[3],
-    bottomWord[0],
-    bottomWord[1],
-    bottomWord[2],
-    bottomWord[3],
-    bottomWord[4]
-  ];
-  return wordArray[index];
-}
-
-function getRandomWord() {
-  const randomNumber = Math.floor(Math.random() * words.length);
-  return words[randomNumber];
+  "",
+  rightWord[3],
+  bottomWord[0],
+  bottomWord[1],
+  bottomWord[2],
+  bottomWord[3],
+  bottomWord[4]
+];
 }
